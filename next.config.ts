b68edb47a180fc1c484f39cbdee44958aea5be7e
Ravindra <ravindra.kumar@dexkor.com>
2026-01-30 10:43:55 +0530

    portfolio completed

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
-const nextConfig = {
-  // Try removing this or changing to 'export'
-  // output: 'standalone', 
-  // OR try:
-  output: 'export', // If you're doing static export
-  
-  images: {
-    unoptimized: true, // Required for static export
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '**',
-      },
-    ],
-  },
-}
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
